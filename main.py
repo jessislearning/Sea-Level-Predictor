ade8a9f55c42d7a558f02f637be0f9279372da24
jessislearning a <jessica.afalla.29@gmail.com>
2024-04-10 15:45:06 +0000

    moved code to sea_level_predictor, code complete

--- main.py
+++ main.py
@@ -1,26 +1,9 @@
 # This entrypoint file to be used in development. Start by reading README.md
-#import sea_level_predictor
-#from unittest import main
+import sea_level_predictor
+from unittest import main
 
 # Test your function by calling it here
-#sea_level_predictor.draw_plot()
+sea_level_predictor.draw_plot()
 
 # Run unit tests automatically
-#main(module='test_module', exit=False)
-
-
-#----my code starts here-----
-import pandas as pd
-import matplotlib.pyplot as plt
-from scipy.stats import linregress
-
-#import data from file
-df = pd.read_csv("epa-sea-level.csv")
-
-fig, ax = plt.subplots(figsize=(8,8))
-scatter = plt.scatter(df["Year"], df["CSIRO Adjusted Sea Level"])
-
-fig.savefig('scatter.png')
-
-
-#print(df.head())
+main(module='test_module', exit=False)
--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -4,18 +4,27 @@ from scipy.stats import linregress
 
 def draw_plot():
     # Read data from file
-
+    df = pd.read_csv("epa-sea-level.csv")
 
     # Create scatter plot
-
+    fig, ax = plt.subplots(figsize=(8,5))
+    scatter = plt.scatter(df["Year"], df["CSIRO Adjusted Sea Level"])
 
     # Create first line of best fit
-
+    # Includes the whole data range
+    line_fit = linregress(df["Year"], df["CSIRO Adjusted Sea Level"])
 
     # Create second line of best fit
-
+    # Fitting from year 2000 to most recent
+    mask = df["Year"] >= 2000
+    line_fit_recent = linregress(df[mask]["Year"], df[mask]["CSIRO Adjusted Sea Level"])
 
     # Add labels and title
+    plt.plot(range(1880, 2051, 1), line_fit.slope*range(1880, 2051, 1)+line_fit.intercept, color="red")
+    plt.plot(range(2000, 2051, 1), line_fit_recent.slope*range(2000, 2051, 1)+line_fit_recent.intercept, color='green')
+    plt.xlabel("Year")
+    plt.ylabel("Sea Level (inches)")
+    plt.title("Rise in Sea Level")
 
     
     # Save plot and return data for testing (DO NOT MODIFY)
